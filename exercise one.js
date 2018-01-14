766a82598f822c6db45456723efe4aa10e873a0f
mnpiazza <32770538+mnpiazza@users.noreply.github.com>
2018-01-14 12:07:12 -0500

    Update exercise one.js

--- exercise one.js
+++ exercise one.js
@@ -1,9 +1,5 @@
-function myFunction() {
-    var greeting = "Enter A Number!";
-    var enter = prompt(greeting, 20);
-    var total = 0;
-    for (var num = 1; num < 20; num++) {
-    total += num;
-}
-document.getElementById("demo").innerHTML = total;
-}
+var enter = prompt('Enter A Number!');
+var total = 0;
+for (var num = 1; num <= 20; num++) {
+total = num + 1;
+console.log(total);
