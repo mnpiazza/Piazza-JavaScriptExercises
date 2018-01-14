8686a58c7cd233175d42a93ebc90641dc5650294
mnpiazza <32770538+mnpiazza@users.noreply.github.com>
2018-01-14 12:20:23 -0500

    Update exercise two.js

--- exercise two.js
+++ exercise two.js
@@ -1,19 +1,9 @@
-var play = "Do you want to play?";
-var enter = prompt(play);
-
-console.log("Output: " + game);
-
-var word = 1;
-
+var play = prompt('Do you want to play?');
+var userInput = "";
+if (play == 'yes') {
 do {
-  var userInput = "Enter a word.";
-  var word = prompt(userInput);
-  word++;
-  var play2 = prompt("Do you want to play again?");
-  if (play2 === "yes") {
-  var info = prompt("Enter a word.");
-  console.log("Output: " + play + " play2);
-} while (word = 1);
-
-if (play2 === "no");
-console.log("Output: " + play + " play2);
+  var word = "Enter a word.";
+  var play2 = ('Do you want to play again?');
+  " " + word++;
+} while (play == 'yes');
+console.log (userInput);
